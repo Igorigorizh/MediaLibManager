d136e0c09758e51ca1a8cef1d7edcf5a1be324cb
Igor Izhov <igor.izhov@gmail.com>
2021-09-03 21:01:07 +0300

    Update myMediaLib_model.py

--- myMediaLib_model.py
+++ myMediaLib_model.py
@@ -1882,7 +1882,7 @@ class PlayerController():
 		try:
 			res = dbl_appl.get_status()['playBack_Mode']
 			print("Player server already running -> no copy allowed.")
-			print("Wait 5 sec for exit..."%(str(res)))
+			print("Wait 5 sec for exit...[%s]"%(str(res)))
 			time.sleep(5)
 
 			return
