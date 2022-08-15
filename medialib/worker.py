7d1ec803f15abafaf07784550cb7377f497a55bd
Igorigorizh <39271356+Igorigorizh@users.noreply.github.com>
2022-08-15 22:36:43 +0300

    celery fix 6

--- medialib/worker.py
+++ medialib/worker.py
@@ -8,3 +8,9 @@ app = Celery(__name__)
 app.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379")
 app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379")
 app.conf.imports = 'medialib'
+
+@app.task(name="hello")
+def hello():
+	time.sleep(10)
+	print("Hello there")
+	return True
