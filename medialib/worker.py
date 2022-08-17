20b370cb8448b53ff364f33ad291860fa90c7ee6
Igorigorizh <39271356+Igorigorizh@users.noreply.github.com>
2022-08-17 03:53:24 +0300

    callback fix 5

--- medialib/worker.py
+++ medialib/worker.py
@@ -44,8 +44,10 @@ music_folders_generation_scheduler = app.task(name='music_folders_generation_sch
 def callback(result):
 	print('Tuta')
 	folderL = result
+	
 	for folder_name in folderL:
 		task_fp_res = app.send_task('get_FP_and_discID_for_album',(folder_name, 0, 'multy', 'FP', 'ACOUSTID_FP_REQ', 'MB_DISCID_REQ'))
+	
 		
 	print(result)
 	
@@ -66,9 +68,10 @@ if __name__ == '__main__':
 	p2 = '/home/medialib/MediaLibManager/music/MUSIC/ORIGINAL_MUSIC/ORIGINAL_CLASSICAL/Vivaldi/Antonio Vivaldi - 19 Sinfonias and Concertos for Strings and Continuo/'
 	task_first_res = app.send_task('music_folders_generation_scheduler-new_recogn_name',(p2,[],[]),link=callback.s())
 	
-			
+	folderL = []		
 	#print(task_first_res.result)
-	folderL = task_first_res.result
+	if task_first_res.result:
+		folderL = task_first_res.result
 	for folder_name in folderL:
 		#task_fp_res = app.send_task('get_FP_and_discID_for_album',(folder_name, 0, 'multy', 'FP', 'ACOUSTID_FP_REQ', 'MB_DISCID_REQ'))
 		pass
