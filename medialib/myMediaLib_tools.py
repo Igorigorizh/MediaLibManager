e0730ecbcf3feea2c610924bea6c84fe8c5df324
Igorigorizh <39271356+Igorigorizh@users.noreply.github.com>
2022-08-19 04:12:26 +0300

    fix 6

--- medialib/myMediaLib_tools.py
+++ medialib/myMediaLib_tools.py
@@ -2537,9 +2537,9 @@ async def acoustID_lookup_wrapper(fp):
 	
 def acoustID_lookup_celery_wrapper(self,fp,*args):
 	print('fp:',fp,'args:',args)
-    API_KEY = 'cSpUJKpD'
-    meta = ["recordings","recordingids","releases","releaseids","releasegroups","releasegroupids", "tracks", "compress", "usermeta", "sources"]
-    return acoustid.lookup(API_KEY, fp[1], fp[0],meta)		
+	API_KEY = 'cSpUJKpD'
+	meta = ["recordings","recordingids","releases","releaseids","releasegroups","releasegroupids", "tracks", "compress", "usermeta", "sources"]
+	return acoustid.lookup(API_KEY, fp[1], fp[0],meta)		
 	
 async def acoustID_lookup_wrapper_parent(fp):
     return await acoustID_lookup_wrapper(fp)	
