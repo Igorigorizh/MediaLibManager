90c87510c71d3a3a9f941ecfc3fc7ef80749b028
Igorigorizh <39271356+Igorigorizh@users.noreply.github.com>
2022-09-16 23:16:48 +0300

    fix: folder tree_buf - 1

--- medialib/myMediaLib_scheduler.py
+++ medialib/myMediaLib_scheduler.py
@@ -48,6 +48,7 @@ musicbrainzngs.set_useragent("python-discid-example", "0.1", "your@mail")
 
 redis_connection = Redis(host=cfg_fp['REDIS']['host'], port=cfg_fp['REDIS']['port'], db=0)
 
+import celery_progress
 
 def music_folders_generation_scheduler(self, folder_node_path, prev_fpDL, prev_music_folderL,*args):	
 	# Генерация линейного списка папок с аудио данным с учетом вложенных папок
@@ -93,7 +94,59 @@ def music_folders_generation_scheduler(self, folder_node_path, prev_fpDL, prev_m
 	print(tmp_music_folderL,len(tmp_music_folderL))
 	return tmp_music_folderL
 	#job_folders_collect = q.enqueue('myMediaLib_tools.find_new_music_folder', [folder_node_path],[],[],'initial')
-	
 
+def get_fp_overall_progress(root_task):
+	
+	total_task_num = len(root_task.children)
+	print('Sub tasks for progress:',total_task_num)
+	i = 0
+	for task_item in root_task.children:
+		if task_item.state == 'SUCCESS':
+			i+=1
+		time.sleep(.1)	
+	return int((i/total_task_num)*100)
+
+def check_job_status_via_result(result):
+	for a in range(100):
+        
+		res = celery_progress.backend.Progress(result).get_info()
+		if res['state'] == 'SUCCESS':
+			break
+		if res['state'] == 'PROGRESS':
+			print(res['progress']['percent'],res['progress']['description'],res['progress']['current'])
+		else:
+			print(res['state'],end=' ')
+		time.sleep(3)
+	print('Sub tasks:',len(result.children))
+	if len(result.children) == 1:
+		for a in range(100):
+			res = celery_progress.backend.Progress(result.children[0]).get_info()
+			if res['state'] == 'SUCCESS':
+				break
+			if res['state'] == 'PROGRESS':
+				print(res['progress']['percent'],res['progress']['description'],res['progress']['current'])
+			else:
+				print(res['state'],end=' ')
+			time.sleep(3)
+			
+		if len(result.children[0].children) >=1:
+			total_task_num = len(result.children[0].children)
+			print('Sub tasks:',total_task_num)
+			i = 0
+			for task_item in result.children[0].children:
+				try:
+					print(task_item.state,task_item.task_id)
+				except:
+					print('Celery connectio error')
+                
+				if task_item.state == 'PROGRESS':
+					res_item = celery_progress.backend.Progress(task_item).get_info()
+					print(res_item)
+				elif task_item.state == 'SUCCESS':
+					res_item = celery_progress.backend.Progress(task_item).get_info()
+					print(res_item['result']['RC'])	
+					i+=1
+				#time.sleep(.1)	
+			print('Progress:', get_fp_overall_progress(result.children[0]))
 
 	
