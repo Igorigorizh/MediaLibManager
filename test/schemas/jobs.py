ff0aebf28b7dfb7f6e1aade0df34ca5bae80d7fb
Igorigorizh <39271356+Igorigorizh@users.noreply.github.com>
2022-10-11 17:43:15 +0300

    medialib package structure -2

--- test/schemas/jobs.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel				
-
-
-#properties required during RQ job creation
-class RqJobCreate(BaseModel):
-	rel_path: str
-	crc32_path: int
-	status: str
